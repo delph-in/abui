9db9cf09bafe6bf6ef30219faccd0101feb45506
Guy Emerson <gete2@cam.ac.uk>
2017-04-15 00:28:23 -0700

    lab3 itsdb, add open class items

--- tsdb/skeletons/Index.lisp
+++ tsdb/skeletons/Index.lisp
@@ -22,4 +22,5 @@
 (
 ((:path . "matrix") (:content . "matrix: A test suite created automatically from the test sentences given in the Grammar Matrix questionnaire."))
 ((:path . "lab2") (:content . "Test suite collected for Lab 2."))
+((:path . "lab3") (:content . "Test suite collected for Lab 3."))
 )
